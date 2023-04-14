03833d7c52b74865492313ffa576ed24f89a8a39
SaerElMasri <saer1890@gmail.com>
2023-04-14 18:26:30 +0200

    Added extra_info API

--- vitality-server/routes/auth.routes.js
+++ vitality-server/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const route = Router();
 
-const register = require('../controllers/auth.controllers');
+const { register, add_extra_info } = require('../controllers/auth.controllers');
 
 route.post('/register', register);
+route.post('/extraInfo', add_extra_info);
 
 module.exports = route
