37ed6ae25da2d2e674fe162832cbf360675415b4
SaerElMasri <saer1890@gmail.com>
2023-04-15 12:15:54 +0200

    Added fetch food logs API

--- vitality-server/controllers/food_log.controllers.js
+++ vitality-server/controllers/food_log.controllers.js
@@ -40,4 +40,48 @@ const addFoodLog = async(req, res) => {
     }
 }
 
-module.exports = addFoodLog
+
+const fetchUserMealLogs = async(req, res) => {
+    const { mealID } = req.body
+    const token = req.header('Authorization')
+    if(!token){
+        return res.status(400).json({
+            status: 400,
+            message: 'Unauthorized'
+        })
+    }
+
+    try{
+        const decode = jwt.decode(token, process.env.JWT_TOKEN);
+        const userID = decode.userId;
+
+        const fetchQuery = 'SELECT * FROM food_intakes WHERE userID = ? AND mealID = ?';
+        const fetchParam = [ userID, mealID ]
+
+        await sql.query(fetchQuery, fetchParam, (err, result) => {
+            if(err){
+                return res.status(500).json({
+                    status: 500,
+                    message: err
+                })
+            }
+            if(result.length === 0) {
+                return res.status(404).json({
+                    status: 404,
+                    message: 'No logs found for the specified mealID'
+                })
+            }
+            res.status(201).json({
+                status: 201,
+                message: result
+            })
+        })
+    }catch(err){
+        res.status(500).json({
+            status: 500,
+            message: err
+        })
+    }
+}
+
+module.exports = { addFoodLog, fetchUserMealLogs }
--- vitality-server/routes/food_log.routes.js
+++ vitality-server/routes/food_log.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const route = Router();
 
-const addFoodLog = require('../controllers/food_log.controllers')
+const { addFoodLog, fetchUserMealLogs} = require('../controllers/food_log.controllers')
 
 route.post('/addLog', addFoodLog);
+route.get('/fetchUserMealLogs', fetchUserMealLogs);
 
 module.exports = route
