2aa3375ede095ff718c5207b5b5a415aa7550576
SaerElMasri <saer1890@gmail.com>
2023-04-15 19:46:36 +0200

    Fixed bugs on API to add runnign log

--- vitality-server/controllers/running_log_session.controllers.js
+++ vitality-server/controllers/running_log_session.controllers.js
@@ -2,25 +2,36 @@ const sql = require('../config/db.connection');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const addNewrunningLog = async(req, res) => {
+const addNewrunningLog = async (req, res) => {
     const { distance, duration, calories_burned, avg_peace } = req.body
 
     const token = req.header('Authorization')
-    if(!token){
-        return res.status(400).json({
-            status: 400,
-            message: 'Unauthorization'
-        })
+    if (!token) {
+      return res.status(400).json({
+        status: 400,
+        message: 'Unauthorized'
+      })
     }
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_TOKEN)
+      const user_id = decoded.userId
 
-    try{
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
-        const user_id = decoded.userId
+      const lastDistance = 'SELECT total_distance FROM running_log_session WHERE user_id = ? ORDER BY created_at DESC LIMIT 1'
+      await sql.query(lastDistance, user_id, async (err, result) => {
+        if(err){
+            return res.status(500).json({
+                status: 500,
+                message: err
+            })
+        }
+        let totalDistance = result[0].total_distance;
+        totalDistance += distance
+        
+        const addQuery = 'INSERT INTO running_log_session SET ?'
+        const addQueryParams = { user_id, distance, calories_burned, avg_peace, duration, total_distance: totalDistance}
 
-        const addSessionQuery = 'INSERT INTO running_log_session SET ?'
-        const addSessionAtt = { user_id, distance, calories_burned, avg_peace, duration}
-
-        await sql.query(addSessionQuery, addSessionAtt, (err) => {
+        await sql.query(addQuery, addQueryParams, (err, result) => {
             if(err){
                 return res.status(500).json({
                     status: 500,
@@ -30,17 +41,22 @@ const addNewrunningLog = async(req, res) => {
 
             res.status(201).json({
                 status: 201,
-                message: "Running Session Logged"
+                message: 'Running Log Added Successfully'
             })
         })
 
-    }catch(err){
-        res.status(500).json({
-            status: 500,
-            message: err
-        })
+
+      })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({
+        status: 500,
+        message: 'Error adding new running log'
+      })
     }
-}
+  }
+  
+
 
 const fetchUserRunningSessions = async(req, res) => {
     const token = req.header('Authorization')
