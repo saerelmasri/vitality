76cd701bc8986238e4bb21b2250febda0548025b
SaerElMasri <saer1890@gmail.com>
2023-04-15 10:52:30 +0200

    Fixed bugs in auth controllers

--- vitality-server/controllers/auth.controllers.js
+++ vitality-server/controllers/auth.controllers.js
@@ -6,30 +6,30 @@ require('dotenv').config();
 const register = async (req, res) => {
     const { nickname, username, phone_number, email, password } = req.body;
     const level = 0;
-
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
     if(!emailRegex.test(email)){
         return res.status(400).json({
             status: 400,
             message: 'Please enter a valid email'
         });
     }
-
     if(!passwordRegex.test(password)){
         return res.status(400).json({
             status: 400,
             message: 'Password must contain at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character'
         });
     }
-
     const hashPassword = await bcrypt.hash(password, 10);
-
     const check_query = 'SELECT * FROM users WHERE email = ?';
     sql.query(check_query, email, (err, result) => {
-        if(err) console.log(err);
-
+        if(err) {
+            console.log(err);
+            return res.status(500).json({
+                status: 500, 
+                message: err
+            });
+        }
         if(result.length > 0){
             return res.status(400).json({
                 status: 400,
@@ -45,19 +45,20 @@ const register = async (req, res) => {
                     message: err
                 });
             }
-
-            const token = jwt.sign({ userId: result.insertId }, process.env.JWT_TOKEN);
-
+            const token = jwt.sign({ 
+                userId: result.insertId
+            }, 
+                process.env.JWT_TOKEN);
             return res.status(200).json({
                 status: 200,
                 message: 'User Successfully Created',
                 token: token
-            });
-        });
-    });
+            })
+        })
+    })
 }
 
-const login = async(req, res) => {
+const login = (req, res) => {
     const { email, password } = req.body;
     const checkUser = 'SELECT * FROM users WHERE email = ?';
     sql.query(checkUser, email, (err, result) => {
@@ -97,4 +98,30 @@ const login = async(req, res) => {
     })
 }
 
-module.exports = { register, login }
+const getUserbyToken = (req, res) => {
+    const token = req.header('Authorization');
+    if(!token){
+        res.status(400).json({
+            status:400,
+            message: 'Unauthorized'
+        });
+    }
+    try{
+        const decode = jwt.verify(token, process.env.JWT_TOKEN);
+        res.status(201).json({
+            status: 201,
+            message: 'Success',
+            response: decode
+        })
+
+    }catch(err){
+        res.status(500).json({
+            message: 'Server Error'
+        });
+    }
+
+
+}
+
+module.exports = { register, login, getUserbyToken }
+
--- vitality-server/routes/auth.routes.js
+++ vitality-server/routes/auth.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const route = Router();
 
-const { register, login } = require('../controllers/auth.controllers');
+const { register, login, getUserbyToken } = require('../controllers/auth.controllers');
 
 route.post('/register', register);
 route.get('/login', login);
+route.get('/getUserbyToken', getUserbyToken);
 
 module.exports = route
