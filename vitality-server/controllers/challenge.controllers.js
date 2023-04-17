fd0b2710d88037ed6afc7f1d30a53b5c9c610031
SaerElMasri <saer1890@gmail.com>
2023-04-17 12:01:22 +0200

    Added API to enrolled user in challenge

--- vitality-server/controllers/challenge.controllers.js
+++ vitality-server/controllers/challenge.controllers.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const sql = require('../config/db.connection');
 const jwt = require('jsonwebtoken')
 
@@ -38,4 +39,57 @@ const createWeeklyChallenge = async (req, res) => {
     })
 }
 
-module.exports = createWeeklyChallenge
+const challenge_enrolled_user = async (req, res) => {
+    const { challenge_id, completation_status, earned_points } = req.body
+    const token = req.header('Authorization')
+    if(!token){
+        res.status(401).json({
+            status: 401,
+            message: 'Unauthorized'
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_TOKEN);
+        const user_id = decoded.userId
+
+        const checkUserChallengeQuery = 'SELECT * FROM challenge_enrolled WHERE user_id = ?'
+        await sql.query(checkUserChallengeQuery, user_id, async(err, result) => {
+            if(err){
+                res.status(500).json({
+                    status: 500,
+                    message: err
+                })
+            }
+            if(result.lenght > 0){
+                res.status(401).json({
+                    status:401,
+                    message: 'User already enrolled in course'
+                })
+            }
+
+            const enrolledUserQuery = 'INSERT INTO challenge_enrolled SET ?'
+            const enrolledUser = { user_id, challenge_id, completation_status, earned_points } 
+
+            await sql.query(enrolledUserQuery, enrolledUser, (err) => {
+                if(err){
+                    res.status(500).json({
+                        status: 500,
+                        message: err
+                    })
+                }
+                res.status(201).json({
+                    status: 201,
+                    message: 'User enrolled'
+                })
+            })
+        })
+    }catch(err){
+        res.status(500).json({
+            status: 500,
+            message: err
+        })
+    }
+}
+
+module.exports = {createWeeklyChallenge, challenge_enrolled_user}
--- vitality-server/routes/challenge.routes.js
+++ vitality-server/routes/challenge.routes.js
@@ -1,7 +1,8 @@
 const { Router } = require('express')
 const route = Router()
-const createWeeklyChallenge = require('../controllers/challenge.controllers')
+const {createWeeklyChallenge, challenge_enrolled_user} = require('../controllers/challenge.controllers')
 
 route.post('/createWeeklyChallenge', createWeeklyChallenge)
+route.post('/enrolled_user', challenge_enrolled_user)
 
 module.exports = route
