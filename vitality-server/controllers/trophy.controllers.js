698ea29be43734e57c47cf39c8ceade1dc30e5d0
SaerElMasri <saer1890@gmail.com>
2023-04-18 18:49:19 +0200

    Fixed API to add trophies

--- vitality-server/controllers/trophy.controllers.js
+++ vitality-server/controllers/trophy.controllers.js
@@ -6,6 +6,47 @@ const createTrophy = async(req, res) => {
     const params = {
         name: req.body.name,
         description: req.body.description,
+        image_url: req.file.filename
+    }
+
+    
+
+    const checkTrophy = 'SELECT * FROM trophies WHERE name = ?'
+    await sql.query(checkTrophy, params.name, async(err, result) => {
+        if(err){
+            return res.status(500).json({
+                status: 500,
+                message: err
+            })
+        }
+
+        if(result.length > 0){
+            return res.status(401).json({
+                status: 401,
+                message: 'Trophy already exists'
+            })
+        }
+        const addTrophyQuery = 'INSERT INTO trophies SET ?'
+        await sql.query(addTrophyQuery, params, (err) => {
+            if(err){
+                return res.status(500).json({
+                    status: 500,
+                    message: err
+                })
+            }
+    
+            return res.status(201).json({
+                status: 201,
+                message: 'Trophy created successfully'
+            })
+        })
+    })    
+}
+
+const updateTrophy = async(req, res) => {
+    const params = {
+        name: req.body.name,
+        description: req.body.description,
         image_url: req.body.image_url
     }
 
--- vitality-server/routes/trophy.routes.js
+++ vitality-server/routes/trophy.routes.js
@@ -1,9 +1,22 @@
 const { Router } = require('express')
 const route = Router()
+const path = require('path')
+const multer = require('multer')
 const jwt_middleware = require('../middleware/jwt.middleware')
+
+const storage = multer.diskStorage({
+    destination: './vitality-server/image/trophies',
+    filename: (req, file, callback) => {
+        return callback(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({
+    storage: storage
+})
 
 const createTrophy = require('../controllers/trophy.controllers')
 
-route.post('/createTrophy', jwt_middleware, createTrophy)
+route.post('/createTrophy', [jwt_middleware, upload.single('image_url')], createTrophy)
 
 module.exports = route
