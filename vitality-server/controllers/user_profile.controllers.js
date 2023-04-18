570803a6c07e7cfa0b417fc891a80eb35e3d8b7b
SaerElMasri <saer1890@gmail.com>
2023-04-18 22:05:06 +0200

    Added API to update info from user table

--- vitality-server/controllers/user_profile.controllers.js
+++ vitality-server/controllers/user_profile.controllers.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const sql = require('../config/db.connection');
 require('dotenv').config();
 
-const update_profile = async(req, res) => {
+const update_profile_extra_info = async(req, res) => {
     const { column_name, valueToUpdate } = req.body
 
     const token = req.header('Authorization')
@@ -46,6 +46,52 @@ const update_profile = async(req, res) => {
 
         })
         
+    }catch(err){
+        res.status(500).json({
+            status: 500,
+            message: err
+        })
+    }
+}
+
+const update_profile_info = async(req, res) => {
+    const { column_name, valueToUpdate } = req.body
+
+    const token = req.header('Authorization')
+    if(!token){
+        return res.status(401).json({
+            status: 401,
+            message: 'Unauthorized'
+        })
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_TOKEN)
+        const user_id = decoded.userId
+        
+        const query = `UPDATE users SET ${column_name} = ? WHERE user_id = ?`
+        console.log(valueToUpdate);
+        await sql.query(query, [ valueToUpdate, user_id ], async(err) => {
+            if(err){
+                return res.status(500).json({
+                    status: 500,
+                    message: err
+                })
+            }
+            const displayQuery = 'SELECT * FROM users WHERE user_id = ?'
+            await sql.query(displayQuery, user_id, (err, result) => {
+                if(err){
+                    return res.status(500).json({
+                        status: 500,
+                        message: err
+                    })
+                }
+                return res.status(201).json({
+                    status: 201,
+                    message: result
+                })
+            })
+        })
     }catch(err){
         res.status(500).json({
             status: 500,
@@ -125,4 +171,4 @@ const user_extra_info = async(req, res) => {
     }
 }
 
-module.exports = {update_profile, user_details, user_extra_info }
+module.exports = {update_profile_extra_info, user_details, user_extra_info, update_profile_info }
--- vitality-server/routes/user_profile.routes.js
+++ vitality-server/routes/user_profile.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
 const route = Router()
-const {update_profile, user_details, user_extra_info} = require('../controllers/user_profile.controllers')
+const {update_profile_extra_info, user_details, user_extra_info, update_profile_info} = require('../controllers/user_profile.controllers')
 
-route.put('/update_profile', update_profile)
+route.put('/update_profile_extra_info', update_profile_extra_info)
+route.put('/update_profile_info', update_profile_info)
 route.get('/user_details', user_details)
 route.get('/user_extra_info', user_extra_info)
 
