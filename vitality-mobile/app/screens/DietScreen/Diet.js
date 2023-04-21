0add6addd9bbd63d72668cd48740bcfdbcfbd04a
SaerElMasri <saer1890@gmail.com>
2023-04-21 14:59:16 +0200

    Added diet type stylings to another file

--- vitality-mobile/app/screens/DietScreen/Diet.js
+++ vitality-mobile/app/screens/DietScreen/Diet.js
@@ -1,5 +1,5 @@
-import { View, Alert, Pressable, Image, Text, StyleSheet, TouchableOpacity } from "react-native"
-import { Color } from "../../../globalStyling"
+import { View, Alert, Pressable, Image, Text } from "react-native"
+import { dietStyling } from "./DietStyling"
 import NextBtn from "../../Components/NextBtn/NextBtn"
 import TypeOfDiet from "../../Components/DietComponent/TypeOfDiet"
 
@@ -33,56 +33,4 @@ const Diet = () => {
         </View>
     )
 }
-
-const dietStyling = StyleSheet.create({
-    mainContainer: {
-        backgroundColor: Color.grey,
-        display: "flex",
-        alignItems: 'center',
-        flex: 1,
-        height: 932,
-        overflow: "hidden",
-        width: "100%",
-    },
-    backBtnContainer: {
-        width: '100%',
-        height: '10%',
-    },
-    backBtn: {
-        height: 50,
-        width: 50,
-        marginTop: 0,
-        marginLeft: 10,
-        marginTop: 10,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    contentContainer: {
-        width: '100%',
-        height: '90%',
-        display: 'flex',
-        alignItems: 'center',
-    },
-    headerContainer: {
-        width: '100%',
-        height: '10%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    headerText: {
-        fontSize: 30,
-        textAlign: 'center',
-        paddingLeft: 20,
-        paddingRight: 20,
-    },
-    dietContainer: {
-        width: '100%',
-        height: '75%',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    }
-})
 export default Diet
---
+++ vitality-mobile/app/screens/DietScreen/DietStyling.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native"
+import { Color } from "../../../globalStyling"
+
+const dietStyling = StyleSheet.create({
+    mainContainer: {
+        backgroundColor: Color.grey,
+        display: "flex",
+        alignItems: 'center',
+        flex: 1,
+        height: 932,
+        overflow: "hidden",
+        width: "100%",
+    },
+    backBtnContainer: {
+        width: '100%',
+        height: '10%',
+    },
+    backBtn: {
+        height: 50,
+        width: 50,
+        marginTop: 0,
+        marginLeft: 10,
+        marginTop: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    contentContainer: {
+        width: '100%',
+        height: '90%',
+        display: 'flex',
+        alignItems: 'center',
+    },
+    headerContainer: {
+        width: '100%',
+        height: '10%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerText: {
+        fontSize: 30,
+        textAlign: 'center',
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    dietContainer: {
+        width: '100%',
+        height: '75%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+})
+
+export { dietStyling }
