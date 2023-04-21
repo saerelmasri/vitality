51859035654d953d6563619010bf89e12811f70a
SaerElMasri <saer1890@gmail.com>
2023-04-21 13:49:41 +0200

    Added goal picker styling to another file

--- vitality-mobile/app/screens/GoalsScreen/Goal.js
+++ vitality-mobile/app/screens/GoalsScreen/Goal.js
@@ -1,5 +1,5 @@
-import { View, Alert, Pressable, Image, Text, TouchableOpacity, StyleSheet } from "react-native"
-import { Color } from "../../../globalStyling"
+import { View, Alert, Pressable, Image, Text } from "react-native"
+import { goalStyling } from "./GoalStyling"
 import NextBtn from "../../Components/NextBtn/NextBtn"
 import GoalBtn from "../../Components/GoalBtn/GoalBtn"
 
@@ -32,75 +32,4 @@ const Goal = () => {
         </View>
     )
 }
-
-const goalStyling = StyleSheet.create({
-    mainContainer: {
-        backgroundColor: Color.grey,
-        display: "flex",
-        alignItems: 'center',
-        flex: 1,
-        height: 932,
-        overflow: "hidden",
-        width: "100%",
-    },
-    backBtnContainer: {
-        width: '100%',
-        height: '10%',
-    },
-    backBtn: {
-        height: 50,
-        width: 50,
-        marginTop: 0,
-        marginLeft: 10,
-        marginTop: 10,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    contentContainer: {
-        borderWidth: 1,
-        width: '100%',
-        height: '90%',
-        display: 'flex',
-        alignItems: 'center',
-    },
-    headerContainer: {
-        width: '100%',
-        height: '10%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    headerText: {
-        fontSize: 30,
-        textAlign: 'center',
-        paddingLeft: 20,
-        paddingRight: 20,
-    },
-    span: {
-        fontWeight: 600
-    },
-    descriptionContainer: {
-        width: '100%',
-        height: '7%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 20
-    },
-    descriptionText: {
-        textAlign: 'center',
-        paddingLeft: 55,
-        paddingRight: 55,
-    },
-    goalContainer: {
-        width: '80%',
-        height: '65%',
-        display: 'flex',
-        justifyContent: 'space-around'
-    }
-})
-
-
-
 export default Goal
---
+++ vitality-mobile/app/screens/GoalsScreen/GoalStyling.js
@@ -0,0 +1,71 @@
+import {  StyleSheet } from "react-native"
+import { Color } from "../../../globalStyling"
+
+const goalStyling = StyleSheet.create({
+    mainContainer: {
+        backgroundColor: Color.grey,
+        display: "flex",
+        alignItems: 'center',
+        flex: 1,
+        height: 932,
+        overflow: "hidden",
+        width: "100%",
+    },
+    backBtnContainer: {
+        width: '100%',
+        height: '10%',
+    },
+    backBtn: {
+        height: 50,
+        width: 50,
+        marginTop: 0,
+        marginLeft: 10,
+        marginTop: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    contentContainer: {
+        width: '100%',
+        height: '90%',
+        display: 'flex',
+        alignItems: 'center',
+    },
+    headerContainer: {
+        width: '100%',
+        height: '10%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    headerText: {
+        fontSize: 30,
+        textAlign: 'center',
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    span: {
+        fontWeight: 600
+    },
+    descriptionContainer: {
+        width: '100%',
+        height: '7%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20
+    },
+    descriptionText: {
+        textAlign: 'center',
+        paddingLeft: 55,
+        paddingRight: 55,
+    },
+    goalContainer: {
+        width: '80%',
+        height: '65%',
+        display: 'flex',
+        justifyContent: 'space-around'
+    }
+})
+
+export { goalStyling }
