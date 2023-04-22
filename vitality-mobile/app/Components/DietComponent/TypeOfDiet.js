03599f355aa1a41f84ae88cfbb20f58ce7d2e0ea
SaerElMasri <saer1890@gmail.com>
2023-04-22 17:01:01 +0200

    Fixed bug to select image dynamically

--- vitality-mobile/app/Components/DietComponent/TypeOfDiet.js
+++ vitality-mobile/app/Components/DietComponent/TypeOfDiet.js
@@ -1,17 +1,38 @@
 import { View, Image, Text, StyleSheet, TouchableOpacity } from "react-native"
 import { Color } from "../../../globalStyling"
+import veganImage from '../../assets/app-img/vegan.png';
+import vegetarianImage from '../../assets/app-img/vegetarian.png';
+import keto from '../../assets/app-img/keto.png';
+import normal from '../../assets/app-img/normal.png'
 
-const TypeOfDiet = ({title, description, imageName}) => {
-    const URL = `../../assets/app-img/${imageName}.png`
-    console.log(URL);
+const TypeOfDiet = ({title, description, imageName, action}) => {
+    let imageSource;
+    switch (imageName) {
+      case 'vegan':
+        imageSource = veganImage;
+        break;
+      case 'vegetarian':
+        imageSource = vegetarianImage;
+        break;
+      case 'keto':
+        imageSource = keto;
+        break;
+      case 'normal':
+        imageSource = normal;
+        break;
+      default:
+        imageSource = null;
+        break;
+    }
+    
     return(
-        <TouchableOpacity style={typeDietStyling.diet} onPress={() => {console.log('Hola');}}>
+        <TouchableOpacity style={typeDietStyling.diet} onPress={action}>
             <View style={typeDietStyling.dietInfo}>
                 <Text style={typeDietStyling.dietTitle}>{title}</Text>
                 <Text style={typeDietStyling.dietDescription}>{description}</Text>
             </View>
             <View style={typeDietStyling.dietImg}>
-                <Image style={typeDietStyling.image} source={{uri: URL}}/>
+                <Image style={typeDietStyling.image} source={imageSource}/>
 
             </View>
         </TouchableOpacity>
--- vitality-mobile/app/screens/DietScreen/Diet.js
+++ vitality-mobile/app/screens/DietScreen/Diet.js
@@ -1,9 +1,14 @@
 import { View, Alert, Pressable, Image, Text } from "react-native"
+import { useRoute } from "@react-navigation/native"
+import { useState } from 'react'
 import { dietStyling } from "./DietStyling"
 import NextBtn from "../../Components/NextBtn/NextBtn"
 import TypeOfDiet from "../../Components/DietComponent/TypeOfDiet"
 
 const Diet = () => {
+    const route = useRoute()
+    const toPass = route.params.data
+    console.log(toPass);
     return(
         <View style={dietStyling.mainContainer}>
             <View  style={dietStyling.backBtnContainer}>
@@ -20,9 +25,9 @@ const Diet = () => {
 
                 <View style={dietStyling.dietContainer}>
                     <TypeOfDiet title={'Ketogenic Diet'} description={'High-fat, low-carb diet that aims to get the body into a state of ketosis.'} imageName={'keto'}/>
-                    <TypeOfDiet title={'Vegan Diet'} description={'Plant-based diet that excludes all animal products including meat, dairy, eggs, and honey.'} imageName={'keto'}/>
-                    <TypeOfDiet title={'Vegetarian Diet'} description={'Plant-based foods and excludes meat, fish, and poultry. It may dairy and eggs.'} imageName={'keto'}/>
-                    <TypeOfDiet title={'Normal Diet'} description={'Balanced and healthy diet that meets the nutritional needs of an individual.'} imageName={'keto'}/>
+                    <TypeOfDiet title={'Vegan Diet'} description={'Plant-based diet that excludes all animal products including meat, dairy, eggs, and honey.'} imageName={'vegan'}/>
+                    <TypeOfDiet title={'Vegetarian Diet'} description={'Plant-based foods and excludes meat, fish, and poultry. It may dairy and eggs.'} imageName={'vegetarian'}/>
+                    <TypeOfDiet title={'Normal Diet'} description={'Balanced and healthy diet that meets the nutritional needs of an individual.'} imageName={'normal'}/>
                     
 
                 </View>
