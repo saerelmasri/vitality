818c40132d0a4af70756a942a7a03319b72d32da
SaerElMasri <saer1890@gmail.com>
2023-04-23 15:50:15 +0200

    Nutrition styling moved to another file

--- vitality-mobile/app/screens/NutritionScreen/Nutrition.js
+++ vitality-mobile/app/screens/NutritionScreen/Nutrition.js
@@ -1,5 +1,5 @@
-import { View, Text, TextInput, Image, Alert, ScrollView, StyleSheet, TouchableOpacity, SafeAreaView } from "react-native";
-import { Color } from "../../../globalStyling";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { nutritionStyle } from "./NutritionStyle";
 import MealContainer from "../../Components/MealCalories/MealCalories";
 import Macro from "../../Components/MacrosTracker/MacrosTracker";
 
@@ -56,83 +56,5 @@ const Nutrition = () => {
             
     )
 }
-
-const nutritionStyle = StyleSheet.create({
-    container: {
-        display: "flex",
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        overflow: "hidden",
-        width: 360,
-    },
-    headerBtnContainer: {
-        width: 360,
-        height: 60,
-        backgroundColor: Color.ligtGreen,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center'
-    },
-    btn: {
-        width: '30%',
-        height: '70%',
-        backgroundColor: Color.grey,
-        borderRadius: 10,
-        display: 'flex', 
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    btnText: {
-        fontSize: 20,
-        fontWeight: 500
-    },
-    bar: {
-        borderWidth: 1, 
-        width: 360,
-        height: 70,
-        display: 'flex',
-        flexDirection: 'row'
-    },
-    left: {
-        borderRightWidth: 1,
-        width: '20%',
-    },
-    center: {
-        width: '60%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    right: {
-        borderLeftWidth: 1,
-        width: '20%',
-    },
-    centerTxt: {
-        fontSize: 30,
-        fontWeight: 500
-    },
-    caloriesStats: {
-        width: 360,
-        height: 450,
-        backgroundColor: Color.ligtGreen,
-        display: 'flex',
-        alignItems: 'center'
-    },
-    textContainer: {
-        width: '100%',
-        height: '10%',
-        display: 'flex',
-        justifyContent: 'center',
-        paddingLeft: 10
-    },
-    text: {
-        fontSize: 30,
-        fontWeight: 500,
-        color: Color.white,
-        paddingLeft: 10
-    }    
-})
 
 export default Nutrition
