1208c4d5807e94cb442a32a26adae6f515d569ae
SaerElMasri <saer1890@gmail.com>
2023-04-24 12:44:52 +0200

    Moved recipe screen styling to another file

--- vitality-mobile/app/screens/RecipesScreen/RecipesScreen.js
+++ vitality-mobile/app/screens/RecipesScreen/RecipesScreen.js
@@ -1,7 +1,7 @@
-import { StyleSheet, View, ScrollView, SafeAreaView, Pressable, Image, StatusBar, Platform, Alert, Text, Dimensions, TextInput, TouchableOpacity, FlatList } from "react-native";
+import { View, ScrollView, SafeAreaView, Pressable, Image, StatusBar, Platform, Alert, Text, TextInput, TouchableOpacity } from "react-native";
 import { Color } from "../../../globalStyling";
+import { recipesStyling } from "./RecipesScreenStyling";
 import RecipeComponent from "../../Components/RecipeComponent/Recipe";
-const { height, width } = Dimensions.get('window')
 
 const Recipes = () => {
     return(
@@ -56,101 +56,5 @@ const Recipes = () => {
     );
 }
 
-const recipesStyling = StyleSheet.create({
-    container: {
-        backgroundColor: Color.ligtGreen,
-        display: "flex",
-        flex: 1,
-        alignItems: 'center',
-        overflow: "hidden",
-        width: "100%",
-    },
-    header: {
-        width: width,
-        height: height / 12,
-        display: 'flex',
-        justifyContent: 'center',
-        paddingLeft: 10
-    },
-    text: {
-        fontSize: 18,
-        fontWeight: 400,
-        color: Color.white
-    },
-    searchContainer: {
-        width: width - 20,
-        height: height / 13,
-        backgroundColor: Color.white,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginLeft: 10,
-        borderRadius: 10
-    },
-    searchIconContainer: {
-        width: width /7,
-        height: '100%',
-    },
-    searchInput: {
-        fontSize: 20,
-        width: '80%',
-        paddingLeft: 10
-    },
-    aiBtn: {
-        width: width - 20,
-        height: height / 6,
-        marginLeft: 10,
-        borderRadius: 10,
-        marginTop: 20,
-        display: 'flex',
-        
-    },
-    chefTxt: {
-        position: 'absolute',
-        zIndex: 1,
-        width: '40%',
-        height: '100%',
-        paddingLeft: '3%',
-        display: 'flex',
-        justifyContent: 'center',
-        opacity: 1
-    },
-    btnContainer: {
-        width: width,
-        height: height / 12,
-        display: 'flex',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        flexDirection: 'row',
-        marginTop: '5%'
-    },
-    btn: {
-        width: '30%',
-        height: '70%',
-        borderRadius: 10,
-        backgroundColor: Color.white,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    btnTxt: {
-        fontSize: 17,
-        fontWeight: 400
-    },
-    recipesContainer: {
-        width: width,
-        height: height / 1,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-
-    },
-    recipesScroll: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: width,
-    }
-})
 
 export default Recipes
