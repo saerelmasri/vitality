5a3becf042cf99d5abab3856c738fdb28d14f80d
SaerElMasri <saer1890@gmail.com>
2023-04-27 14:34:34 +0200

    BackEnd: added controller to remove user

--- vitality-server/routes/auth.routes.js
+++ vitality-server/routes/auth.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const route = Router();
 
-const { register, login, getUserbyToken, getUserInfo } = require('../controllers/auth.controllers');
+const { register, login, getUserbyToken, getUserInfo, removeUser } = require('../controllers/auth.controllers');
 
 route.post('/register', register);
 route.post('/login', login);
 route.get('/getUserbyToken', getUserbyToken);
 route.get('/getUserInfobyToken', getUserInfo);
+route.delete('/removeUser', removeUser)
 
 module.exports = route
