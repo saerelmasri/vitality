9898a21d63adee60f3a525e1d4385ffe4ef84a60
SaerElMasri <saer1890@gmail.com>
2023-04-27 13:55:58 +0200

    Backend: Added routes to daily calories and modified routes of leaderboard

--- vitality-server/routes/food_log.routes.js
+++ vitality-server/routes/food_log.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const route = Router();
 
-const { addFoodLog, fetchUserMealLogs, getCaloriesIntakeByDate} = require('../controllers/food_log.controllers')
+const { addFoodLog, fetchUserMealLogs, getCaloriesIntakeByDate, addDailyCalories, getDailyCalories} = require('../controllers/food_log.controllers')
 
 route.post('/addLog', addFoodLog);
+route.post('/addDailyCalories', addDailyCalories);
 route.get('/fetchUserMealLogs', fetchUserMealLogs);
 route.get('/getCaloriesIntakeByDate', getCaloriesIntakeByDate);
+route.get('/getDailyCalories', getDailyCalories);
 
 module.exports = route
--- vitality-server/routes/leaderboard.routes.js
+++ vitality-server/routes/leaderboard.routes.js
@@ -2,9 +2,8 @@ const { Router } = require('express')
 const route = Router()
 const jwt_middleware = require('../middleware/jwt.middleware')
 
-const {getLeaderRunning, getLeaderLevel} = require('../controllers/leaderboard.controllers')
+const {getLeaderLevel} = require('../controllers/leaderboard.controllers')
 
-route.get('/running_leaderboard', jwt_middleware, getLeaderRunning)
 route.get('/level_leaderboard', jwt_middleware, getLeaderLevel)
 
 module.exports = route
