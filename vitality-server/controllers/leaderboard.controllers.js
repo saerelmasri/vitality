43b28c69a20104cb3415c23363fe0a03ea2309a4
SaerElMasri <saer1890@gmail.com>
2023-04-27 13:51:10 +0200

    BackEnd: Added controller to add daily calories to user

--- vitality-server/controllers/leaderboard.controllers.js
+++ vitality-server/controllers/leaderboard.controllers.js
@@ -1,29 +1,5 @@
 const sql = require('../config/db.connection');
 
-const getLeaderRunning = async (req, res) => {
-    try{
-        const query = 'SELECT u.id, u.full_name, ua.highest_distance FROM users u JOIN ( SELECT ua.user_id, MAX(ua.total_distance) as highest_distance FROM running_log_session ua GROUP BY ua.user_id) ua ON u.id = ua.user_id ORDER BY ua.highest_distance DESC;'
-        await sql.query(query, (err, result) => {
-            if(err){
-                return res.status(500).json({
-                    status: 500,
-                    message: err
-                })
-            }
-
-            return res.status(201).json({
-                status: 201,
-                message: result
-            })
-        })
-    }catch(err){
-        res.status(500).json({
-            status:500,
-            message: err
-        })
-    }
-}
-
 const getLeaderLevel = async(req, res) => {
     try{
 
@@ -51,4 +27,4 @@ const getLeaderLevel = async(req, res) => {
     }
 }
 
-module.exports = {getLeaderRunning, getLeaderLevel}
+module.exports = {getLeaderLevel}
--- vitality-server/routes/running_log_session.route.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express')
-const route = Router()
-const {addNewrunningLog, fetchUserRunningSessions} = require('../controllers/running_log_session.controllers')
-
-route.post('/addNewrunningLog', addNewrunningLog);
-route.get('/fetchUserRunningSessions', fetchUserRunningSessions);
-
-module.exports = route
